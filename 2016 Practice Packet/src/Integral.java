b698a9e0d450dd4ded2df96481d4574fe8b1c295
Eldrojin <dystraxus@gmail.com>
2016-09-21 09:07:11 -0500

    Finished Integral and Periodic
    almost done with war.

--- 2016 Practice Packet/src/Integral.java
+++ 2016 Practice Packet/src/Integral.java
@@ -11,7 +11,15 @@ public class Integral {
         int sets = Integer.parseInt(in.nextLine().trim());
 
         while (sets-- > 0) {
-
+            String[] line = in.nextLine().split(" ");
+            double num1 = Double.parseDouble(line[0]);
+            double num2 = Double.parseDouble(line[1]);
+            double num3 = Double.parseDouble(line[2]);
+            double sum = 0;
+            for (; num1 < num2; num1 += num3) {
+                sum += (3 * Math.pow(num1, 2) + (2 * num1) + 1) * num3;
+            }
+            System.out.printf("%.6f%n", sum);
         }
     }
 }
