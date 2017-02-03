bb470e5c68dc2bd50eb166a40d78867342c4f1b1
Long Nguyen <dystraxus@gmail.com>
2017-02-03 07:43:21 -0600

    Work More on Beads

--- USACO/src/beads.java
+++ USACO/src/beads.java
@@ -1,7 +1,7 @@
 /*
 ID: 18nguye1
 LANG: JAVA
-PROG: 
+PROG: beads
 */
 
 import java.io.*;
@@ -16,14 +16,15 @@ public class beads {
         st = new StringTokenizer(in.readLine());
         String necklaceRead = st.nextToken();
         char[] necklace = necklaceRead.toCharArray();
-        int max = 1;
+        int max = 0;
         int start = (necklaceRead.indexOf('r') < necklaceRead.indexOf('b')) ?
                 necklaceRead.indexOf('r') : necklaceRead.indexOf('b');
-        counter += start;
         int breakk = 0;
         int maxx = 0;
         char temp = necklaceRead.charAt(start);
-        for (int i = start; i < 29; i++) {
+        for (int i = start; i < counter + counter; i++) {
+//            System.out.print(necklace[start]);
+//            System.out.print(temp);
             if (necklace[start] == temp || necklace[start] == 'w') {
                 max++;
             } else {
@@ -32,15 +33,17 @@ public class beads {
                 }
                 breakk = max;
                 max = 1;
-                temp = necklaceRead.charAt(i);
+                temp = necklace[start];
             }
-            System.out.println(max);
+//            System.out.println("Max: " + max + "\nBreakk: " + breakk);
             char tmp = necklace[necklace.length - 1];
             for (int j = necklace.length - 1; j > 0; j--) {
                 necklace[j] = necklace[j - 1];
             }
             necklace[0] = tmp;
         }
+//        System.out.println();
+//        System.out.println(necklaceRead);
         out.println(maxx);
         out.close();
     }
