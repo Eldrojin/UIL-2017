a8fc752cb4a06d913175dc3950039473d5e413af
Eldrojin <dystraxus@gmail.com>
2017-02-10 08:43:43 -0600

    Work on USACO beads

--- USACO/src/beads.java
+++ USACO/src/beads.java
@@ -17,30 +17,42 @@ public class beads {
         String necklaceRead = st.nextToken();
         char[] necklace = necklaceRead.toCharArray();
         int max = 0;
-        int start = (necklaceRead.indexOf('r') < necklaceRead.indexOf('b')) ?
-                necklaceRead.indexOf('r') : necklaceRead.indexOf('b');
+        int start = 1;
         int breakk = 0;
         int maxx = 0;
         char temp = necklaceRead.charAt(start);
-        for (int i = start; i < counter + counter; i++) {
+        for (int i = start; i < counter + counter + 1; i++) {
 //            System.out.print(necklace[start]);
 //            System.out.print(temp);
+            if (max == necklace.length) {
+                maxx = max;
+                break;
+            }
             if (necklace[start] == temp || necklace[start] == 'w') {
                 max++;
             } else {
                 if (max + breakk > maxx) {
                     maxx = max + breakk;
                 }
-                breakk = max;
                 max = 1;
+                for (int j = start; j > 0; j--) {
+                    max += 1;
+                }
+//                if (necklace[start - 1] == 'w') {
+//                    max = 2;
+//                }
+                breakk = max;
                 temp = necklace[start];
             }
-//            System.out.println("Max: " + max + "\nBreakk: " + breakk);
+            System.out.println("Max: " + max + "\nBreakk: " + breakk);
             char tmp = necklace[necklace.length - 1];
             for (int j = necklace.length - 1; j > 0; j--) {
                 necklace[j] = necklace[j - 1];
             }
             necklace[0] = tmp;
+        }
+        if (max + breakk > maxx) {
+            maxx = max + breakk;
         }
 //        System.out.println();
 //        System.out.println(necklaceRead);
